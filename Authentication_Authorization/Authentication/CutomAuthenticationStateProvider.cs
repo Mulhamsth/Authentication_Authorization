6840c17c832016794988ef6aa8cfbb2cf249b8dd
Mulhamsth <mulhamsth@gmail.com>
2023-04-30 16:09:31 +0200

    GetAuthenicationStateAsync Method

--- Authentication_Authorization/Authentication/CutomAuthenticationStateProvider.cs
+++ Authentication_Authorization/Authentication/CutomAuthenticationStateProvider.cs
@@ -1,6 +1,44 @@
-﻿namespace Authentication_Authorization.Authentication
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace Authentication_Authorization.Authentication
 {
-	public class CutomAuthenticationStateProvider
+	public class CutomAuthenticationStateProvider : AuthenticationStateProvider
 	{
+		private readonly ProtectedSessionStorage _sessionStorage;
+		private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+		public CutomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage)
+		{
+			_sessionStorage = sessionStorage;
+		}
+
+		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			try
+			{
+				var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+				var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+
+				if (userSession == null)
+					return await Task.FromResult(new AuthenticationState(_anonymous));
+
+				var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+				{
+					new Claim(ClaimTypes.Name, userSession.UserName),
+					new Claim(ClaimTypes.Role, userSession.Role)
+				}, "CustomAuth"));
+
+				return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+			}
+			catch
+			{
+				return await Task.FromResult(new AuthenticationState(_anonymous));
+			}
+			
+		}
+
+
 	}
 }
